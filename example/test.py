061c0e2a0bcc7ed76fddb521af36dc3f686d0bb8
Hunter_LC <lc411887055@gmail.com>
2020-01-17 23:42:27 +0800

    添加文本词性标注，新增名词、动词、代词个数特征

--- example/test.py
+++ example/test.py
@@ -17,10 +17,28 @@
 # print("Urls: ", Find(string))
 
 
-from snownlp import sentiment
-from snownlp import SnowNLP
+# from snownlp import sentiment
+# from snownlp import SnowNLP
+#
+# # sentiment.train(r'E:\PythonCode\Features\util\negative.txt',r'E:\PythonCode\Features\util\positive.txt')
+# # sentiment.save(r'E:\PythonCode\Features\util\sentiment.marshal')
+# s = SnowNLP('那个人真让人不舒服 ')
+# print(s.sentiments)
 
-# sentiment.train(r'E:\PythonCode\Features\util\negative.txt',r'E:\PythonCode\Features\util\positive.txt')
-# sentiment.save(r'E:\PythonCode\Features\util\sentiment.marshal')
-s = SnowNLP('那个人真让人不舒服 ')
-print(s.sentiments)
+import jieba.posseg as pseg
+import re
+words =pseg.cut("".join(re.findall(u"[\u4e00-\u9fa5]","【外卖配送员无证驾驶闯红灯924次面临十几万罚款】近日，广东茂名民警在对一辆闯红灯的外卖配送摩托进行核查时大吃一惊：该车仅半年就闯红灯924次！平均每天五六次，最多时一天闯红灯十八次。按道路交通安全法，该驾驶员要为他900多次闯红灯的行为缴纳十几万元的罚款。中国新闻网...（中国新闻网）")))
+n = 0
+r = 0
+v = 0
+for w in words:
+    print(w.word,w.flag)
+    if(w.flag.startswith('n')):
+        n += 1
+    elif(w.flag.startswith('v')):
+        v += 1
+    elif(w.flag.startswith('r')):
+        r += 1
+print("名词个数：",n)
+print("动词个数：",v)
+print("代词个数：",r)
--- work/feature_extraction.py
+++ work/feature_extraction.py
@@ -6,8 +6,11 @@ import time
 import jieba
 from snownlp import sentiment
 from snownlp import SnowNLP
+import jieba.posseg as pseg
+
 
 train_csv_path = r'G:\毕设\数据集\微博\train.csv'
+text_csv_path = r'G:\毕设\数据集\微博\text.csv'
 train_negative_corpus_path = os.path.abspath(os.path.dirname(os.getcwd())+os.path.sep+".")+'/util/negative.txt'
 train_positive_corpus_path = os.path.abspath(os.path.dirname(os.getcwd())+os.path.sep+".")+'/util/positive.txt'
 sentiment_model_path = os.path.abspath(os.path.dirname(os.getcwd())+os.path.sep+".")+'/util/sentiment.marshal'
@@ -30,7 +33,15 @@ def train_data_read(train_csv_path):
     print("数据载入完成")
     return df_text,df_user,df_image
 
-def text_insert_cols(df_text):
+def text_data_read():
+    '''
+    文本特征文件的读取
+    :return: 文本特征文件
+    '''
+    df_text = pd.read_csv(text_csv_path)
+    return df_text
+
+def text_insert_cols(df_text,new_features_list):
     '''
     增加文本新的特征列，方便后续提取并补充值
     :param df_text: 文本信息
@@ -38,41 +49,60 @@ def text_insert_cols(df_text):
     '''
     print("正在扩展文本新特征列...")
     col_name = list(df_text.columns)
-    new_features_name = ['text_length','contains_questmark','num_questmarks','contains_exclammark',
-                         'num_exclammarks','contains_hashtag','num_hashtags','contains_URL',
-                         'num_URLs','contains_mention','num_mentions','sentiment_score']
-    col_name = col_name[0:2]+ new_features_name +col_name[2:]
+    col_name = col_name[0:-2]+ new_features_list +col_name[-2:]
     df_text = df_text.reindex(columns=col_name, fill_value=0)
     print("文本新特征列扩展完成...")
     return df_text
 
 def text_feature_extraction(df_text):
     print("开始文本特征提取...")
-    #统计字符串长度
-    df_text['text_length'] = df_text['text'].str.len()
-    #将情感分数列转为float
-    df_text['sentiment_score'] = df_text['sentiment_score'].astype(float)
-    #其余数据统计
+    # #统计字符串长度
+    # df_text['text_length'] = df_text['text'].str.len()
+    # #将情感分数列转为float
+    # df_text['sentiment_score'] = df_text['sentiment_score'].astype(float)
+    # #其余数据统计
     i = 0
     for index, row in df_text.iterrows():
         print("处理进度",i+1,"/",df_text.shape[0])
         #获得需要处理的文本内容
         text_content = row['text']
-        #获得是否含有问号以及问号的数量
-        df_text.at[i,'contains_questmark'], df_text.at[i,'num_questmarks'] = text_questmark(text_content)
-        #获得是否含有感叹号以及感叹号的数量
-        df_text.at[i, 'contains_exclammark'], df_text.at[i, 'num_exclammarks'] = text_exclammark(text_content)
-        #获得是否含有hashtag以及hashtag的数量
-        df_text.at[i, 'contains_hashtag'], df_text.at[i, 'num_hashtags'] = text_hashtag(text_content)
-        #获得是否含有url以及url的数量
-        df_text.at[i, 'contains_URL'], df_text.at[i, 'num_URLs'] = text_url(text_content)
-        #获得是否含有@以及@的数量
-        df_text.at[i, 'contains_mention'], df_text.at[i, 'num_mentions'] = text_mention(text_content)
-        #获得文本情感分数
-        df_text.at[i, 'sentiment_score'] = text_sentiment_score(text_content)
+        # #获得是否含有问号以及问号的数量
+        # df_text.at[i,'contains_questmark'], df_text.at[i,'num_questmarks'] = text_questmark(text_content)
+        # #获得是否含有感叹号以及感叹号的数量
+        # df_text.at[i, 'contains_exclammark'], df_text.at[i, 'num_exclammarks'] = text_exclammark(text_content)
+        # #获得是否含有hashtag以及hashtag的数量
+        # df_text.at[i, 'contains_hashtag'], df_text.at[i, 'num_hashtags'] = text_hashtag(text_content)
+        # #获得是否含有url以及url的数量
+        # df_text.at[i, 'contains_URL'], df_text.at[i, 'num_URLs'] = text_url(text_content)
+        # #获得是否含有@以及@的数量
+        # df_text.at[i, 'contains_mention'], df_text.at[i, 'num_mentions'] = text_mention(text_content)
+        # #获得文本情感分数
+        # df_text.at[i, 'sentiment_score'] = text_sentiment_score(text_content)
+        #词性标注，统计名词、动词、代词数量并返回
+        df_text.at[i, 'num_noun'],df_text.at[i, 'num_verb'],df_text.at[i, 'num_pronoun'] = text_part_of_speech(text_content)
         i += 1
     print("文本特征提取结束...")
     return df_text
+
+def text_part_of_speech(text_content):
+    '''
+    将文本中的汉字进行词性标注并返回数量
+    :param text_content: 文本信息
+    :return: n名词数量,v动词数量,r代词数量
+    '''
+    #选取所有的汉字
+    words = pseg.cut("".join(re.findall(u"[\u4e00-\u9fa5]",text_content)))
+    n = 0 #名词数量
+    r = 0 #代词数量
+    v = 0 #动词数量
+    for w in words:
+        if (w.flag.startswith('n')):
+            n += 1
+        elif (w.flag.startswith('v')):
+            v += 1
+        elif (w.flag.startswith('r')):
+            r += 1
+    return n,v,r
 
 def text_questmark(text_content):
     '''
@@ -183,16 +213,43 @@ def text_mention(text_content):
     else:
         return 0,0
 
+
+# start = time.time()
+# #原始数据的读入
+# # df_text,df_user,df_image = train_data_read(train_csv_path)
+#
+# #微博文本扩展特征数据列
+# # new_text_features_list = ['text_length', 'contains_questmark', 'num_questmarks', 'contains_exclammark',
+# #                      'num_exclammarks', 'contains_hashtag', 'num_hashtags', 'contains_URL',
+# #                      'num_URLs', 'contains_mention', 'num_mentions', 'sentiment_score','num_noun','num_verb','num_pronoun']
+# df_text = text_insert_cols(df_text,new_text_features_list)
+# #情感分析语料模型训练
+# # text_train_sentiment()
+# # 读入停用词表
+# stopwords = get_stopwords_list()
+#
+# df_text = text_feature_extraction(df_text)
+# df_text.to_csv(text_csv_path,index=0)#不保留行索引
+# end = time.time()
+# print("运行时间：",end-start)
+# # df_user.to_csv(r'G:\毕设\数据集\微博\user.csv',index=0)#不保留行索引
+# # df_image.to_csv(r'G:\毕设\数据集\微博\image.csv',index=0)#不保留行索引
+
+
 start = time.time()
-df_text,df_user,df_image = train_data_read(train_csv_path)
-df_text = text_insert_cols(df_text)
+#原始数据的读入
+df_text = text_data_read()
+
+#微博文本扩展特征数据列
+# new_text_features_list = ['num_noun','num_verb','num_pronoun']
+# df_text = text_insert_cols(df_text,new_text_features_list)
 #情感分析语料模型训练
 # text_train_sentiment()
 # 读入停用词表
 stopwords = get_stopwords_list()
 
 df_text = text_feature_extraction(df_text)
-df_text.to_csv(r'G:\毕设\数据集\微博\text.csv',index=0)#不保留行索引
+df_text.to_csv(text_csv_path,index=0)#不保留行索引
 end = time.time()
 print("运行时间：",end-start)
 # df_user.to_csv(r'G:\毕设\数据集\微博\user.csv',index=0)#不保留行索引
